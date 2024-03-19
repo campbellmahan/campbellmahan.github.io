155a37d0c0251159e9a6479a61baab33c1bd8822
campbellmahan <cama8022@colorado.edu>
2024-03-19 10:35:40 -0600

    3/19 updates

---
+++ ica/ica10/ica10.js
@@ -0,0 +1,28 @@
+// const button = document.querySelectorAll("button");
+// console.log(button)
+
+// for (const b in button) {
+    // b.addEventListener('click', changeText);
+// }
+// button.addEventListener('click', changeText);
+
+// const heading = document.querySelector("h1");
+
+// function changeText() {
+    // heading.textContent = 'hi!';
+    // const head = prompt('tell me a secret');
+    // heading.textContent = head;
+// }
+
+const button1 = document.querySelector("button1");
+button1.addEventListener('click', changeText);
+
+const button2 = document.getElementById("button2");
+button2.addEventListener('click', changeText);
+
+const heading = document.querySelector("h1")
+
+function changeText() {
+    const head = prompt("tell me a secret");
+    heading.textContent = head;
+}
