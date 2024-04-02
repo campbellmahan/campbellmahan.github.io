50bcb80eeaa3f396f5b0be0d5c5bdeea29ec8023
campbellmahan <cama8022@colorado.edu>
2024-04-02 14:02:04 -0600

    Update wa11.js

--- wa/wa11/wa11.js
+++ wa/wa11/wa11.js
@@ -6,6 +6,15 @@ const overlay = document.querySelector('.overlay');
 
 /* Declaring the array of image filenames */
 
+const images = ['pic1.jpg', 'pic2.jpg', 'pic3.jpg', 'pic4.jpg', 'pic5.jpg'];
+const alts = {
+    'pic1.jpg' : 'closeup of a human eye',
+    'pic2.jpg' : 'rock',
+    'pic3.jpg' : 'flowers',
+    'pic4.jpg' : 'wall', 
+    'pic5.jpg' : 'moth',
+}
+
 /* Declaring the alternative text for each image file */
 
 /* Looping through images */
@@ -16,3 +25,16 @@ newImage.setAttribute('alt', xxx);
 thumbBar.appendChild(newImage);
 
 /* Wiring up the Darken/Lighten button */
+
+btn.addEventListener('click', () => {
+    const btnClass = btn.getAttribute('class');
+    if (btnClass === 'dark') {
+        btn.setAttribute('class', 'light');
+        overlay.computedStyleMap.backgroundColor = 'rgba(0,0,0,0.5)';
+    }
+    else {
+        btn.setAttribute('class', 'dark');
+        btn.textContent = 'Darken';
+        overlay.computedStyleMap.backgroundColor = 'rgba(0,0,0,0)';
+    }
+});
