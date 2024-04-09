b735116aca09f0cc8e606dac54caef78a1ad1f6d
campbellmahan <cama8022@colorado.edu>
2024-04-09 10:48:41 -0600

    4/9 changes

---
+++ ica/ica12/ica12.js
@@ -0,0 +1,42 @@
+const btn = document.querySelector("#js-new-quote");
+btn.addEventListener('click', newQuote);
+
+const answerBtn = document.querySelector("#js-tweet");
+btn.addEventListener('click', getAnswer);
+
+const answerText = document.querySelector("#js-answer-text");
+
+const endpoint = 'https://trivia.cyberwisp.com/getrandomchristmasquestion';
+
+let answer = '';
+
+async function newQuote() {
+    // console.log("Test"); 
+    try {
+        const response = await fetch (endpoint);
+        if(!response.ok) {
+            throw Error(response.ststausText)
+        }
+
+        const json = await response.json();
+        console.log(json['question']);
+        displayQuote(json['question']);
+        console.log(json['answer']);
+        answer = json('answer');
+        answerText,textContent = '';
+    } catch (err) {
+        console.log (err);
+        alert('Failed to fetch new quote');
+    }
+}
+
+function displayQuote(quote) {
+    const quoteText = document.querySelector("#js-quote-text");
+    quoteText.textContent = quote; 
+}
+
+function getAnswer() {
+    answerText.textContext = answer; 
+}
+
+getQuote();
