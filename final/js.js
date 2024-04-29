e3d158030325b1a846270fd24c29542b74d637f1
campbellmahan <cama8022@colorado.edu>
2024-04-29 12:13:46 -0600

    4/29 changes

--- final/js.js
+++ final/js.js
@@ -4,11 +4,30 @@ function updateVolumeDisplay() {
     document.getElementById("volume-display").innerText = `Volume: ${currentVolume}`;
 }
 
+const phrases = [
+    "Better luck next time",
+    "You're bad at this",
+    "Change the volume already", 
+    "Boo hoo",
+    "Make it louder",
+    "That's too loud.. quieter",
+];
+
 function createBall() {
     const ball = document.createElement("div");
     ball.className = "ball";
     ball.style.left = `${Math.random() * window.innerWidth}px`;
     ball.style.top = `${Math.random() * window.innerHeight}px`;
+    document.getElementById("balls-container").appendChild(ball);
+
+    const text = document.createElement("p");
+    const randomIndex = Math.floor(Math.random() * phrases.length);
+    text.textContent = phrases[randomIndex];
+
+    text.classList.add("ball-text");
+
+    ball.appendChild(text);
+
     document.getElementById("balls-container").appendChild(ball);
 }
 
